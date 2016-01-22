3889e8bbcd0519d528ab7602f5adf70f82f88577
Ian Burgmyer <spectere@gmail.com>
2016-01-21 21:03:12 -0500

    Removed all the whitespace! :D

--- bf.c
+++ bf.c
@@ -2,30 +2,8 @@
 #define m 30000
 #define _ P[i]==
 #define S M[p]
-main(int z,char**Z) {
-	FILE* f;
-	long c,L[1000];
-	int p=0,l=-1,i=m,n=0;
-	
-	f=fopen(Z[1],"r");
-
-	fseek(f,0,2);
-	c=ftell(f);
-	rewind(f);
-	
-	char P[c],M[m];
-	for(;i>=0;M[--i]=0);
-	fread(P,c,c,f);
-	fclose(f);
-
-	for(; i++ < c;
-		n?(_ 91?n++:_ 93?n--:n)
-		:(_ 43?S++
-		:_ 45?S--
-		:_ 46?putchar(S)
-		:_ 44?S=getchar()
-		:_ 60?(--p<0?p=m:p)
-		:_ 62?(++p==m?p=0:p)
-		:_ 91?(S?L[++l]=i:n++)
-		:_ 93?(S?i=L[l]:(l>=0?l--:l)):i));
-}
+main(int z,char**Z){FILE*f;long c,L[1000];int p=0,l=-1,i=m,n=0;f=fopen(Z[1]
+,"r");fseek(f,0,2);c=ftell(f);rewind(f);char P[c],M[m];for(;i>=0;M[--i]=0);
+fread(P,c,c,f);fclose(f);for(;i++<c;n?(_ 91?n++:_ 93?n--:n):(_ 43?S++:_ 45?
+S--:_ 46?putchar(S):_ 44?S=getchar():_ 60?(--p<0?p=m:p):_ 62?(++p==m?p=0:p)
+:_ 91?(S?L[++l]=i:n++):_ 93?(S?i=L[l]:(l>=0?l--:l)):i));}
