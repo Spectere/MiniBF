f7d1f5a5e0a742366b0badaea6c54ba51f5aef50
Ian Burgmyer <spectere@gmail.com>
2016-01-21 20:50:37 -0500

    Removed unnecessary space in function declaration.

--- bf.c
+++ bf.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #define m 30000
 #define _ P[i]==
-main(int z,char** Z) {
+#define S M[p]
+main(int z,char**Z) {
 	FILE* f;
 	long c,L[1000];
 	int p=0,l=-1,i=m,n=0;
@@ -19,12 +20,12 @@ main(int z,char** Z) {
 
 	for(; i++ < c;
 		n?(_ 91?n++:_ 93?n--:n)
-		:(_ 43?M[p]++
-		:_ 45?M[p]--
-		:_ 46?putchar(M[p])
-		:_ 44?M[p]=getchar()
+		:(_ 43?S++
+		:_ 45?S--
+		:_ 46?putchar(S)
+		:_ 44?S=getchar()
 		:_ 60?(--p<0?p=m:p)
 		:_ 62?(++p==m?p=0:p)
-		:_ 91?(M[p]?L[++l]=i:n++)
-		:_ 93?(M[p]?i=L[l]:(l>=0?l--:l)):i));
+		:_ 91?(S?L[++l]=i:n++)
+		:_ 93?(S?i=L[l]:(l>=0?l--:l)):i));
 }
