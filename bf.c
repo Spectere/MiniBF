a912a29d203e6b03d1c5a870eddc6e6f828e0a6d
Ian Burgmyer <spectere@gmail.com>
2016-01-21 20:37:21 -0500

    Removed some spaces.

--- bf.c
+++ bf.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
+#include<stdio.h>
 #define m 30000
 main(int z,char** Z) {
 	FILE* f;
-	long c, L[1000];
-	int p = 0, l = -1, i = m, n = 0;
+	long c,L[1000];
+	int p=0,l=-1,i=m,n=0;
 	
-	f = fopen(Z[1], "r");
+	f=fopen(Z[1],"r");
 
-	fseek(f, 0, 2);
-	c = ftell(f);
+	fseek(f,0,2);
+	c=ftell(f);
 	rewind(f);
 	
-	char P[c], a, M[m];
-	for(; i >= 0; M[--i] = 0);
-	fread(P, c, c, f);
+	char P[c],a,M[m];
+	for(;i>=0;M[--i]=0);
+	fread(P,c,c,f);
 	fclose(f);
 
 	for(; i++ < c;
